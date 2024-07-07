a0b9f585deec3ebf6976e7c68cecc9c73a3fed10
Piyush Kumar <piyushkumar1116@gmail.com>
2024-07-08 00:01:12 +0530

    Pk/tic tac toe (#3)
    * esay level and 2 player add
    
    * medium level is added
    
    * refactor and add player name input
    
    * refactor and add player name input
    
    * hard level

---
+++ src/components/tic-tac-toe/components/ticTacToeContainer.tsx
@@ -0,0 +1,220 @@
+'use client';
+import { SiteLayout } from '@/layout';
+import React, { useEffect, useState } from 'react';
+import useTicTacToe from '../hooks/useTicTacToe';
+import { gamePlayerType, levelType } from '../types/level.type';
+
+const TicTacToeContainer = () => {
+  const [gamePlayer, setGamePlayer] = useState<gamePlayerType>(null);
+  const [level, setLevel] = useState<levelType>(null);
+  const [player, setPlayer] = useState<{ [key: string]: string }>({
+    X: 'Player A',
+    O: 'Player B',
+  });
+
+  const [isStarted, setIsStarted] = useState<boolean>(false);
+
+  const {
+    board,
+    turn,
+    setboard,
+    winner,
+    setScores,
+    handleClick,
+    getStatusMessage,
+    resetGame,
+    getComputerMove,
+  } = useTicTacToe();
+
+  if (gamePlayer === 1 && turn === 'O' && !winner) {
+    setTimeout(() => {
+      const index = getComputerMove(board, level);
+      handleClick(index, 'O');
+    }, 1000); // Delay of 1000 milliseconds (1 second)
+  }
+
+  const goToStart = () => {
+    resetGame();
+    setIsStarted(false);
+    setGamePlayer(null);
+    setLevel(null);
+    setScores({ X: 0, O: 0 });
+  };
+
+  const response = getStatusMessage();
+
+  return (
+    <SiteLayout className="bg-slate-300 min-h-[90vh]">
+      <div className="flex flex-col justify-center items-center gap-4 p-8">
+        <h1 className="text-3xl font-bold">TicTacToe</h1>
+
+        {!gamePlayer && (
+          <>
+            <div className="flex gap-4 text-white ">
+              <button
+                onClick={() => {
+                  setGamePlayer(1);
+                  setPlayer({ X: 'Player A', O: 'AI Player' });
+                }}
+                className="sm:text-lg text-sm font-semibold shadow-md  rounded-xl bg-green-700 hover:bg-green-600 cursor-pointer px-4 py-2"
+              >
+                1 Player Game
+              </button>
+              {gamePlayer !== 1 && (
+                <button
+                  onClick={() => {
+                    setGamePlayer(2);
+                    setPlayer({ X: 'Player A', O: 'Player B' });
+                  }}
+                  className="sm:text-lg text-sm font-semibold shadow-md rounded-xl bg-green-700 hover:bg-green-600 cursor-pointer px-4 py-2"
+                >
+                  2 Player Game
+                </button>
+              )}
+            </div>
+          </>
+        )}
+
+        {gamePlayer === 1 && !level && !isStarted && (
+          <>
+            <div className="flex flex-col gap-4">
+              <p>Player Name</p>
+              <input
+                type="text"
+                required
+                value={player.X}
+                className="h-10 rounded-lg px-2"
+                onChange={(e) => setPlayer({ ...player, X: e.target.value })}
+              />
+            </div>
+            <div className="flex gap-4">
+              <button
+                onClick={() => {
+                  setLevel('easy');
+                  setIsStarted(true);
+                }}
+                className="sm:text-lg text-sm text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500"
+              >
+                Esay
+              </button>
+              <button
+                onClick={() => {
+                  setLevel('medium');
+                  setIsStarted(true);
+                }}
+                className="sm:text-lg text-sm text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500"
+              >
+                Medium
+              </button>
+              <button
+                onClick={() => {
+                  setLevel('hard');
+                  setIsStarted(true);
+                }}
+                className="sm:text-lg text-sm text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500"
+              >
+                Hard
+              </button>
+            </div>
+            {/* <button className='text-xl text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500' onClick={() => setIsStarted(true)}>Start</button> */}
+          </>
+        )}
+        {gamePlayer === 2 && !isStarted && (
+          <>
+            <div className="flex flex-col gap-4">
+              <p>Player Name</p>
+              <input
+                type="text"
+                required
+                value={player.X}
+                className="h-10 rounded-lg px-2"
+                onChange={(e) => setPlayer({ ...player, X: e.target.value })}
+              />
+              <input
+                type="text"
+                required
+                value={player.O}
+                className="h-10 rounded-lg px-2"
+                onChange={(e) => setPlayer({ ...player, O: e.target.value })}
+              />
+            </div>
+            <button
+              className="text-xl text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500"
+              onClick={() => setIsStarted(true)}
+            >
+              Start
+            </button>
+          </>
+        )}
+
+        {isStarted && (
+          <>
+            {/* <p>Player {gamePlayer} level {level}</p> */}
+            <div className="font-medium text-gray-800">
+              <h1 className="text-xl font-bold text-center">
+                Game Points {level ? '(' + level.toUpperCase() + ')' : ''}
+              </h1>
+              <p className="text-center">
+                {player.X || 'X'}: Points {response.score.X} & {player.O || 'O'}
+                : Points {response.score.O}
+              </p>
+            </div>
+            {response.draw && (
+              <div className="text-2xl font-medium text-gray-800">Draw</div>
+            )}
+
+            {!response.draw && response.winner && (
+              <div className="text-2xl font-medium text-gray-800">
+                {player[response.winner]} is winner
+              </div>
+            )}
+
+            {!response.draw && !response.winner && (
+              <div className="text-2xl font-medium text-gray-800">
+                {player[response.turn]} Turn
+              </div>
+            )}
+
+            {/* Game Grid Board */}
+            <div className="grid grid-cols-3 gap-4">
+              {board.map((b, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="text-6xl font-semibold border-2 rounded-xl hover:bg-slate-200 cursor-pointer shadow-md"
+                  >
+                    <button
+                      className="h-28 w-28 hover:animate-spin"
+                      onClick={() => handleClick(i, turn)}
+                      disabled={b !== null}
+                    >
+                      {b}
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+
+            {/* Game Control */}
+            <div className="flex gap-4">
+              <button
+                className="sm:text-lg text-sm text-white font-medium bg-blue-600 px-8 py-2 rounded-md hover:bg-blue-500"
+                onClick={goToStart}
+              >
+                Go to Start
+              </button>
+              <button
+                onClick={resetGame}
+                className="sm:text-lg text-sm text-white font-medium bg-red-600 px-8 py-2 rounded-md hover:bg-red-500"
+              >
+                Reset Game
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </SiteLayout>
+  );
+};
+
+export default TicTacToeContainer;
--- src/components/tic-tac-toe/hooks/useTicTacToe.ts
+++ src/components/tic-tac-toe/hooks/useTicTacToe.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { levelType, turnType } from '../types/level.type';
 
 const initialBoard = () => Array(9).fill(null);
@@ -14,35 +14,45 @@ const WINNING_PATTERNS = [
   [2, 4, 6],
 ];
 
-const calculateWinner = (currentBoard: Array<string | null>): string | null => {
-  for (let i = 0; i < WINNING_PATTERNS.length; i++) {
-    const [a, b, c] = WINNING_PATTERNS[i];
-
-    if (
-      currentBoard[a] &&
-      currentBoard[a] === currentBoard[b] &&
-      currentBoard[a] === currentBoard[c]
-    ) {
-      return currentBoard[a];
-    }
-  }
-
-  return null;
-};
-
-const chekWinProbability = (board: Array<string | null>, turn: string) => {
-  return WINNING_PATTERNS.some((pattern) =>
-    pattern.every((index) => board[index] === turn)
-  );
-};
-
 const useTicTacToe = () => {
   const [board, setboard] = useState(initialBoard());
   const [turn, setTurn] = useState<turnType>('X');
+  const [winner, setWinner] = useState<string | null>(null);
+  const [socres, setScores] = useState<{ [key: string]: number }>({
+    X: 0,
+    O: 0,
+  });
+
+  const calculateWinner = useCallback(
+    (currentBoard: Array<string | null>): string | null => {
+      for (let i = 0; i < WINNING_PATTERNS.length; i++) {
+        const [a, b, c] = WINNING_PATTERNS[i];
+
+        if (
+          currentBoard[a] &&
+          currentBoard[a] === currentBoard[b] &&
+          currentBoard[a] === currentBoard[c]
+        ) {
+          return currentBoard[a];
+        }
+      }
+
+      return null;
+    },
+    [board]
+  );
+
+  const chekWinProbability = useCallback(
+    (board: Array<string | null>, turn: string) => {
+      return WINNING_PATTERNS.some((pattern) =>
+        pattern.every((index) => board[index] === turn)
+      );
+    },
+    []
+  );
 
   const handleClick = (index: number, turn: turnType) => {
     //check for winner
-    const winner = calculateWinner(board);
     if (winner || board[index]) return;
 
     const newBoard = [...board];
@@ -51,100 +61,196 @@ const useTicTacToe = () => {
     setTurn(turn === 'X' ? 'O' : 'X');
   };
 
-  const getComputerMove = (
+  const checkWinner = (board: Array<string | null>): string | null => {
+    for (let pattern of WINNING_PATTERNS) {
+      const [a, b, c] = pattern;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+
+    if (board.every((cell) => cell !== null)) {
+      return 'tie';
+    }
+
+    return null;
+  };
+  // Minimax algorithm
+  const minimax = (
     board: Array<string | null>,
-    level: levelType
+    depth: number,
+    isMaximizing: boolean,
+    player: string,
+    opponent: string
   ): number => {
-    // console.log(`computer move ${level} & turn:${turn} & board ${board}`);
-    if (level === 'easy') {
-      // console.log('level easy');
-      const emptySquares = board.reduce((acc, square, index) => {
-        if (square === null) {
-          acc.push(index);
-        }
-        return acc;
-      }, [] as number[]);
+    let scores = { [player]: 10, [opponent]: -10, tie: 0 };
 
-      const randomIndex = Math.floor(Math.random() * emptySquares.length);
-      return emptySquares[randomIndex];
-    } else if (level === 'medium') {
-      // console.log('level medium');
-      const opponent = turn === 'X' ? 'O' : 'X';
+    const winner = checkWinner(board);
 
-      // Check for a winning move
+    console.log(`Winner: ${winner} `);
+    if (winner !== null) {
+      return scores[winner];
+    }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
       for (let i = 0; i < board.length; i++) {
         if (board[i] === null) {
-          board[i] = turn;
-          if (chekWinProbability(board, turn)) {
-            // console.log(`move: ${turn} ${i} winning for me`);
-            board[i] = null;
-            return i;
-          }
+          board[i] = player;
+          let score = minimax(board, depth + 1, false, player, opponent);
           board[i] = null;
+          bestScore = Math.max(score, bestScore);
         }
       }
-
-      // Check for a blocking move
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
       for (let i = 0; i < board.length; i++) {
         if (board[i] === null) {
           board[i] = opponent;
-          if (chekWinProbability(board, opponent)) {
-            // console.log(`move: ${opponent} ${i} block win`);
-            board[i] = null;
-            return i;
-          }
+          let score = minimax(board, depth + 1, true, player, opponent);
           board[i] = null;
+          bestScore = Math.min(score, bestScore);
         }
       }
-
-      // Pick a random available index
-      const emptySquares = board.reduce((acc, square, index) => {
-        if (square === null) {
-          acc.push(index);
-        }
-        return acc;
-      }, [] as number[]);
-
-      const randomIndex = Math.floor(Math.random() * emptySquares.length);
-      // console.log(`move random: ${turn} index: ${emptySquares[randomIndex]}`);
-      return emptySquares[randomIndex];
-    } else if (level === 'hard') {
-      // console.log('level hard');
-      return 0;
+      return bestScore;
     }
-    return 0; // should never happen
   };
 
+  const getEasyMove = (board: Array<string | null>) => {
+    const emptySquares = board.reduce((acc, square, index) => {
+      if (square === null) {
+        acc.push(index);
+      }
+      return acc;
+    }, [] as number[]);
+
+    const randomIndex = Math.floor(Math.random() * emptySquares.length);
+    return emptySquares[randomIndex];
+  };
+
+  const getComputerMove = useCallback(
+    (board: Array<string | null>, level: levelType): number => {
+      // console.log(`computer move ${level} & turn:${turn} & board ${board}`);
+      if (level === 'easy') {
+        return getEasyMove(board);
+      } else if (level === 'medium') {
+        // Check for a winning move
+        for (let i = 0; i < board.length; i++) {
+          if (board[i] === null) {
+            board[i] = 'O';
+            if (chekWinProbability(board, 'O')) {
+              // console.log(`move: ${"O"} ${i} winning for me`);
+              board[i] = null;
+              return i;
+            }
+            board[i] = null;
+          }
+        }
+
+        // Check for a blocking move
+        for (let i = 0; i < board.length; i++) {
+          if (board[i] === null) {
+            board[i] = 'X';
+            if (chekWinProbability(board, 'X')) {
+              // console.log(`move: ${"X"} ${i} block win`);
+              board[i] = null;
+              return i;
+            }
+            board[i] = null;
+          }
+        }
+
+        // Pick a random available index
+        return getEasyMove(board);
+      } else if (level === 'hard') {
+        const player = 'O'; // Assume computer is 'O'
+        const opponent = 'X';
+        let bestScore = -Infinity;
+
+        let move = -1;
+
+        console.log(`computer move initial: ${move} score: ${bestScore}`);
+
+        for (let i = 0; i < board.length; i++) {
+          if (board[i] === null) {
+            board[i] = player;
+            let score = minimax(board, 0, false, player, opponent);
+            board[i] = null;
+            console.log(`**INSIDE BOARD: ${i} score: ${score}`);
+            if (score > bestScore) {
+              console.log(`**********computer move: ${i} score: ${score}`);
+              bestScore = score;
+              move = i;
+            }
+          }
+        }
+
+        if (move === -1) {
+          move = getEasyMove(board);
+        }
+
+        console.log(`computer move: ${move} score: ${bestScore}`);
+        return move;
+      }
+      return 0; // should never happen
+    },
+    []
+  );
+
+  const handleScore = (winner: string | null) => {
+    if (winner) {
+      setScores({
+        ...socres,
+        [winner]: socres[winner] + 5,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleScore(winner);
+  }, [winner]);
+
+  useEffect(() => {
+    const winner = calculateWinner(board);
+    if (winner) {
+      setWinner(winner);
+    }
+  }, [calculateWinner]);
   const getStatusMessage = (): {
     winner: string | null;
     draw: boolean;
     turn: turnType;
+    score: { [key: string]: number };
   } => {
-    const winner = calculateWinner(board);
-
-    if (winner)
+    if (winner) {
       return {
         winner,
         draw: false,
         turn,
+        score: socres,
       };
+    }
 
     if (!board.includes(null))
       return {
         winner: null,
         draw: true,
         turn,
+        score: socres,
       };
 
     return {
       winner: null,
       draw: false,
       turn,
+      score: socres,
     };
   };
 
   const resetGame = () => {
     setboard(initialBoard());
+    setWinner(null);
     setTurn('X');
   };
 
@@ -156,6 +262,8 @@ const useTicTacToe = () => {
     getStatusMessage,
     resetGame,
     getComputerMove,
+    winner,
+    setScores,
   };
 };
 
